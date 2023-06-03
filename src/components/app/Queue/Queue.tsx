a7920070daee8166fed7cd1d6bba7ccd5144166b
Hoa Ngo <hoango1607@gmail.com>
2023-06-03 22:35:54 +0930

    feat: create queue list

--- src/components/app/Queue/Queue.tsx
+++ src/components/app/Queue/Queue.tsx
@@ -1,7 +1,7 @@
 const Queue = () => {
   return (
     <div className="min-h-[300px] flex-grow bg-[#111927]">
-      <div className="flex items-center justify-between">
+      <div className="mb-2 flex items-center justify-between border-b border-b-[#6366f1] p-3">
         <p className="h6">Queue</p>
         <div>
           <input
@@ -12,12 +12,55 @@ const Queue = () => {
             className=" mx-2 rounded-none bg-transparent p-1 text-sm font-medium transition duration-200 hover:bg-[#4338ca]"
             type="button"
           >
-            Send
+            Get
           </button>
         </div>
       </div>
-      <div className="">
-        <div>gg</div>
+      <div className="h-full max-h-[390px] overflow-y-auto">
+        <div>
+          <ul>
+            <li className="border-be-ytb-item flex items-center justify-between py-3">
+              <img
+                className="mx-3 h-[48px]"
+                src="https://play-lh.googleusercontent.com/RslBy1o2NEBYUdRjQtUqLbN-ZM2hpks1mHPMiHMrpAuLqxeBPcFSAjo65nQHbTA53YYn"
+                alt="Thumbnail"
+              />
+              <div className="flex-1">
+                <div>
+                  PHỎNG VẤN MAN UNITED - MAN CITY: TEN HAG QUYẾT TÂM VÔ ĐỊCH,
+                  PEP GUARDIOLA THẬN TRỌNG PHỎNG VẤN MAN UNITED - MAN CITY: TEN
+                </div>
+                <span className="rounded-sm border border-[rgba(157,164,222,0.43)] px-1 text-xs">
+                  2:30
+                </span>
+              </div>
+              <div className="mr-3 flex flex-row gap-1">
+                <button type="button">Delete</button>
+                <button type="button">Play</button>
+              </div>
+            </li>
+            <li className="flex items-center justify-between py-3">
+              <img
+                className="mx-3 h-[48px]"
+                src="https://play-lh.googleusercontent.com/RslBy1o2NEBYUdRjQtUqLbN-ZM2hpks1mHPMiHMrpAuLqxeBPcFSAjo65nQHbTA53YYn"
+                alt="Thumbnail"
+              />
+              <div className="flex-1">
+                <div>
+                  PHỎNG VẤN MAN UNITED - MAN CITY: TEN HAG QUYẾT TÂM VÔ ĐỊCH,
+                  PEP GUARDIOLA THẬN TRỌNG
+                </div>
+                <span className="rounded-sm border border-[rgba(157,164,222,0.43)] px-1 text-xs">
+                  2:30
+                </span>
+              </div>
+              <div className="mr-3 flex flex-row gap-1">
+                <button type="button">Delete</button>
+                <button type="button">Play</button>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
