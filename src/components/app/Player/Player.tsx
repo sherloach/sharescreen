f20041239188adf6d3b53593308e41fef52be9e7
Hoa Ngo <hoango1607@gmail.com>
2023-06-06 16:08:22 +0930

    feat: add react youtube

--- src/components/app/Player/Player.tsx
+++ src/components/app/Player/Player.tsx
@@ -1,7 +1,23 @@
+import YouTube, { YouTubeProps } from 'react-youtube';
+
 const Player = () => {
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+  const opts: YouTubeProps['opts'] = {
+    height: '400',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
   return (
     <div className="h-[400px]">
-      <p className="text-white">Player</p>
+      <YouTube videoId="3_JE76PKBWE" opts={opts} onReady={onPlayerReady} />
     </div>
   );
 };
