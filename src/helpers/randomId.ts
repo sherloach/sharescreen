a1ab6941b997eae4b5118aa6c25287052c0ae2b7
Hoa Ngo <hoango1607@gmail.com>
2023-06-07 22:31:21 +0930

    feat: add randomid function

---
+++ src/helpers/randomId.ts
@@ -0,0 +1,7 @@
+const randomId = () => {
+  const dateString = Date.now().toString(36);
+  const randomness = Math.random().toString(36).substr(2);
+  return dateString + randomness;
+};
+
+export default randomId;
